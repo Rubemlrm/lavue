3adbf91eca62d67208ba00e24f255a6e293f2682
Rubem Mota <rubemmota89@gmail.com>
2016-10-15 13:39:01 +0100

    updated travis and gitIgnore Starting working on template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,32 +11,74 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  rel="stylesheet" type="text/css">
+         <link rel="stylesheet" href="css/app.css"  rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="listRoles flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
+      <div class="container">
+        <div id="loginbox-template" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info" >
+                    <div class="panel-heading">
+                        <div class="panel-title">Login</div>
+                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Esqueceu sua senha?</a></div>
+                    </div>
 
-            <roles></roles>
+                    <div style="padding-top:30px" class="panel-body" >
 
-            <template id="roles-template">
-                <ul class="list-group">
-                    <li class="list-group-item" v-for="role in list">@{{role.name}}</li>
-                </ul>
+                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 
-            </template>
+                        <form id="loginform" class="form-horizontal" role="form">
 
-            <div class="content">
-                <div class="title m-b-md">
-                <ul class="list-group">
+                            <div style="margin-bottom: 25px" class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="usuário">
+                                    </div>
 
-                </ul>
-            </div>
+                            <div style="margin-bottom: 25px" class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                        <input id="login-password" type="password" class="form-control" name="password" placeholder="senha">
+                                    </div>
+
+
+
+                        <!--    <div class="input-group">
+                                    <!--  <div class="checkbox">
+                                        <label>
+                                          <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
+                                        </label>
+                                      </div>
+                                    </div> -->
+
+
+                                <div style="margin-top:10px" class="form-group">
+                                    <!-- Button -->
+
+                                    <div class="col-sm-12 controls">
+                                      <a id="btn-login" href="#" class="btn btn-success">Login  </a>
+                                      <!--<a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>-->
+
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="col-md-12 control">
+                                        <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                                            Não tem cadastro!
+                                        <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
+                                            Cadastre-se aqui
+                                        </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+
+
+                        </div>
+                    </div>
         </div>
+         </div>
+    </div>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://unpkg.com/vue@2.0.1/dist/vue.js" ></script>
         <script src="js/main.js"></script>
